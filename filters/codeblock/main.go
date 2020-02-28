08f7e58063df8976f58fc0302a9be8adeeada16a
Luke Huang <lukehuang.ca@gmail.com>
2020-02-27 22:32:03 -0500

    improvement

--- filters/codeblock/main.go
+++ filters/codeblock/main.go
@@ -11,6 +11,7 @@ import (
 	"github.com/vupiggy/pandoc-filter/codeblock/figure"
 	"github.com/vupiggy/pandoc-filter/codeblock/amsthm"
 	"github.com/vupiggy/pandoc-filter/codeblock/code"
+	"github.com/vupiggy/pandoc-filter/codeblock/choice"
 	pf "github.com/oltolm/go-pandocfilters"
 )
 
@@ -24,11 +25,13 @@ func Insert(cb codeblock.CodeBlock, target string, content string) interface{} {
 var fig figure.Figure
 var thm amsthm.Theorem
 var cod code.Code
+var cho code.Choice
 
 var cbMap = map[string]codeblock.CodeBlock {
 	"figure"  : &fig,
 	"theorem" : &thm,
 	"snippet" : &cod,
+	"choice"  : &cho
 }
 
 func processCB(key string, value interface{}, target string, meta interface{}) interface{} {
